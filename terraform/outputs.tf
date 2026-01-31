f05e4196e832e694b0a56de78245a6efc16d44a3
VijayPal4601 <79791310+VijayPal4601@users.noreply.github.com>
2026-01-31 16:44:38 +0530

    Update outputs.tf
    Updated output [skip ci]

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,11 @@ output "registry_url" {
   description = "Artifact Registry URL"
   value       = "${var.region}-docker.pkg.dev/${var.project_id}/${var.repo_name}"
 }
+
+output "gke_cluster_name" {
+  value = google_container_cluster.gke.name
+}
+
+output "gke_zone" {
+  value = var.zone
+}
