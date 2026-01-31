c55678c1a7b67286d95277f769825339d6eff016
VijayPal4601 <79791310+VijayPal4601@users.noreply.github.com>
2026-01-31 16:44:02 +0530

    Update main.tf
    Added gke [skip ci]

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,3 +14,37 @@ resource "google_artifact_registry_repository" "gcr_repo" {
   depends_on    = [google_project_service.artifactregistry]
 }
 
+resource "google_project_service" "gke" {
+  service = "container.googleapis.com"
+}
+
+resource "google_project_service" "artifactregistry" {
+  service = "artifactregistry.googleapis.com"
+}
+
+resource "google_container_cluster" "gke" {
+  name     = var.gke_cluster_name
+  location = var.zone
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  depends_on = [
+    google_project_service.gke
+  ]
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "primary-node-pool"
+  cluster    = google_container_cluster.gke.name
+  location   = var.zone
+  node_count = 2
+
+  node_config {
+    machine_type = "e2-medium"
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
